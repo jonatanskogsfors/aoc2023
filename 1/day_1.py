37fb853dd867998b6f5a0cdd213b2e5a615f4abd
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2023-12-01 14:31:25 +0100

    Day 1: **

--- 1/day_1.py
+++ 1/day_1.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 NUMBERS = {
-    "zero": 0,
     "one": 1,
     "two": 2,
     "three": 3,
@@ -18,44 +17,44 @@ def parse_input(path: Path):
     return path.read_text().strip("\n").split("\n")
 
 
-def decode_row(row: str, only_true_digits = False) -> int:
-    buffer = ""
-    numbers = []
-    for character in row:
-        if character.isdigit():
-            numbers += _numbers_in_buffer(buffer)
-            buffer = ""
-            numbers.append(character)
-
-        elif not only_true_digits:
-            buffer += character
-    numbers += _numbers_in_buffer(buffer)
-    return int(numbers[0] + numbers[-1])
+def decode_calibration(row: str, allow_words=False) -> int:
+    return int(first_number(row, allow_words) + last_number(row, allow_words))
 
 
-def _numbers_in_buffer(buffer: str) -> list[str]:
-    found_numbers = []
-    for name, value in NUMBERS.items():
-        position = buffer.find(name)
-        if position != -1:
-            found_numbers.append((position, value))
-    return [str(value) for _, value in sorted(found_numbers)]
+def first_number(row, allow_words=False) -> str:
+    for n in range(len(row)):
+        for name, value in NUMBERS.items():
+            substring = row[: n + 1]
+            if str(value) in substring:
+                return str(value)
+            if allow_words and name in substring:
+                return str(value)
+    return None
+
+
+def last_number(row, allow_words=False):
+    for n in range(len(row)):
+        for name, value in NUMBERS.items():
+            substring = row[-(n + 1) :]
+            if str(value) in substring:
+                return str(value)
+            if allow_words and name in substring:
+                return str(value)
+    return None
 
 
 def solve_part_one(input_path: Path) -> int:
     parsed_input = parse_input(input_path)
-    numbers = [decode_row(row, True) for row in parsed_input]
+    numbers = [decode_calibration(row) for row in parsed_input]
     return sum(numbers)
+
 
 def solve_part_two(input_path: Path) -> int:
     parsed_input = parse_input(input_path)
-    numbers = [decode_row(row) for row in parsed_input]
+    numbers = [decode_calibration(row, allow_words=True) for row in parsed_input]
     return sum(numbers)
 
-#53363 High
-#53343 High
-print(solve_part_one(Path("1/input.txt")))
-print(solve_part_two(Path("1/input.txt")))
 
-for row in parse_input(Path("1/input.txt")):
-    print(f"{decode_row(row)}: {row}")
+if __name__ == "__main__":
+    print(solve_part_one(Path("1/input.txt")))
+    print(solve_part_two(Path("1/input.txt")))  # 53363 -> 53343 -> _53340_
--- 1/test_1.py
+++ 1/test_1.py
@@ -17,20 +17,15 @@ def test_parse_input_returns_list_of_strings():
 
 
 @pytest.mark.parametrize(
-    "given_code, expected_number",
-    (
-        ("1abc2", 12),
-        ("pqr3stu8vwx", 38),
-        ("a1b2c3d4e5f", 15),
-        ("treb7uchet", 77)
-    )
+    "given_code, expected_value",
+    (("1abc2", 12), ("pqr3stu8vwx", 38), ("a1b2c3d4e5f", 15), ("treb7uchet", 77)),
 )
-def test_decode_row_gives_expected_values(given_code, expected_number):
-    assert day_1.decode_row(given_code) == expected_number
+def test_decode_calibration_gives_expected_values(given_code, expected_value):
+    assert day_1.decode_calibration(given_code) == expected_value
 
 
 @pytest.mark.parametrize(
-    "given_code, expected_number", 
+    "given_code, expected_value",
     (
         ("two1nine", 29),
         ("eightwothree", 83),
@@ -40,20 +35,44 @@ def test_decode_row_gives_expected_values(given_code, expected_number):
         ("zoneight234", 14),
         ("7pqrstsixteen", 76),
         ("two934seven1", 21),
-    )
+        ("59onefourxnmptgtcone", 51),
+        ("fourcdqc7vvqhpgqlkjsevenfivefiveseven", 47),
+        ("24kmxnlpqxgpsevenfoursixfour", 24),
+    ),
 )
-def test_decode_row_handles_spelled_out_numbers(given_code, expected_number):
-    assert day_1.decode_row(given_code) == expected_number
+def test_decode_calibration_handles_spelled_out_numbers(given_code, expected_value):
+    assert day_1.decode_calibration(given_code, allow_words=True) == expected_value
 
 
 @pytest.mark.parametrize(
-    "given_code, expected_number", 
+    "given_code, expected_value",
     (
         ("seven3lbcvjxqhhdpzkttqsixjzzjjbclfq1fiveeightwojx", 72),
-    )
+        ("oneight", 18),
+        ("twone", 21),
+        ("threeight", 38),
+        ("fiveight", 58),
+        ("sevenine", 79),
+        ("eightwo", 82),
+        ("eighthree", 83),
+        ("nineight", 98),
+    ),
 )
-def test_decode_row_handles_overlapping_numbers(given_code, expected_number):
-    assert day_1.decode_row(given_code) == expected_number
+def test_decode_calibration_handles_overlapping_numbers(given_code, expected_value):
+    assert day_1.decode_calibration(given_code, allow_words=True) == expected_value
+
+
+@pytest.mark.parametrize(
+    "given_code, expected_value",
+    (
+        ("38sevennineninemnfzklttkxnine3", 33),
+        ("onetwo3two", 12),
+    ),
+)
+def test_decode_row_handles_multiple_instances_of_same_number(
+    given_code, expected_value
+):
+    assert day_1.decode_calibration(given_code, allow_words=True) == expected_value
 
 
 def test_solving_part_1_gives_expected_value():
