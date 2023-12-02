102e27ae46cbaf3341ee8043a66efdb7b78433b0
Jonatan Skogsfors <jonatan@skogsfors.net>
2023-12-02 11:17:42 +0100

    Day 2: **

---
+++ 2/day_2.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+from functools import reduce
+from pathlib import Path
+import re
+
+GAME_HEADER_PATTERN = re.compile(r"Game (\d+)")
+
+
+def main():
+    input_path = Path("input/input_2.txt")
+    print(solve_part_one(input_path))
+    print(solve_part_two(input_path))
+
+
+def parse_input(input_path: Path):
+    game_rows = input_path.read_text().strip().split("\n")
+    games = {}
+    for game_row in game_rows:
+        game_header, game = game_row.split(":")
+        if games_header_match := GAME_HEADER_PATTERN.match(game_header):
+            game_id = int(games_header_match.group(1))
+            games[game_id] = game.strip()
+    return games
+
+
+def is_game_possible(game: str, rule: dict) -> bool:
+    total_colors = max_by_color(game)
+    return all(items <= rule[color] for color, items in total_colors.items())
+
+
+def max_by_color(game: str) -> dict:
+    max_colors = defaultdict(int)
+    for subset in game.split(";"):
+        for color_cubes in subset.strip().split(","):
+            cubes, color = color_cubes.strip().split(" ")
+            color = color.lower()
+            cubes = int(cubes)
+            max_colors[color] = max(cubes, max_colors[color])
+    return max_colors
+
+
+def solve_part_one(input_path: Path) -> int:
+    parsed_games = parse_input(input_path)
+    rule = {"red": 12, "green": 13, "blue": 14}
+    possible_games = [
+        game_id
+        for game_id, game in parsed_games.items()
+        if is_game_possible(game, rule)
+    ]
+    return sum(possible_games)
+
+
+def solve_part_two(input_path: Path) -> int:
+    parsed_games = parse_input(input_path)
+    powers = [
+        reduce(lambda a, b: a * b, max_by_color(game).values())
+        for game in parsed_games.values()
+    ]
+
+    return sum(powers)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2/test_2.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+import pytest
+
+import day_2
+
+
+def test_parse_input_returns_a_dictionary_of_strings():
+    given_path = Path("test_input_1.txt")
+    parsed_input = day_2.parse_input(given_path)
+
+    assert parsed_input
+    assert isinstance(parsed_input, dict)
+    assert all(isinstance(key, int) for key in parsed_input)
+    assert all(isinstance(value, str) for value in parsed_input.values())
+
+
+@pytest.mark.parametrize(
+    "given_game, expected_status",
+    (
+        ("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", True),
+        ("1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", True),
+        ("8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", False),
+        ("1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", False),
+        ("6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", True),
+    ),
+)
+def test_is_game_possible_should_return_the_correct_status(given_game, expected_status):
+    given_rule = {"red": 12, "green": 13, "blue": 14}
+    assert day_2.is_game_possible(given_game, given_rule) == expected_status
+
+
+@pytest.mark.parametrize(
+    "given_game, expected_fewest",
+    (
+        (
+            "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            {"red": 4, "green": 2, "blue": 6},
+        ),
+        (
+            "1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            {"red": 1, "green": 3, "blue": 4},
+        ),
+        (
+            "8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            {"red": 20, "green": 13, "blue": 6},
+        ),
+        (
+            "1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            {"red": 14, "green": 3, "blue": 15},
+        ),
+        (
+            "6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+            {"red": 6, "green": 3, "blue": 2},
+        ),
+    ),
+)
+def test_fewest_cubes_for_game_returns_expected_vlue(given_game, expected_fewest):
+    assert day_2.max_by_color(given_game) == expected_fewest
+
+
+def test_solving_part_1_gives_expected_value():
+    given_input_path = Path("test_input_1.txt")
+    answer = day_2.solve_part_one(given_input_path)
+    assert answer == 8
+
+
+def test_solving_part_2_gives_expected_value():
+    given_input_path = Path("test_input_1.txt")
+    answer = day_2.solve_part_two(given_input_path)
+    assert answer == 2286
