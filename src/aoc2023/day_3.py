18330e0aca5857b05787802d681548d1d8d9fb8c
Jonatan Skogsfors <jonatan@skogsfors.net>
2023-12-03 09:43:18 +0100

    Day 3: **

---
+++ src/aoc2023/day_3.py
@@ -0,0 +1,105 @@
+import re
+from pathlib import Path
+from typing import NamedTuple
+
+NUMBER_PATTERN = re.compile(r"(\d+)")
+
+
+class ItemPosition(NamedTuple):
+    value: int | str
+    x: int
+    y: int
+
+
+def main():
+    input_path = Path("input/input_3.txt")
+    print(solve_part_one(input_path))
+    print(solve_part_two(input_path))
+
+
+def parse_input(input_path: Path) -> tuple[str]:
+    return tuple(input_path.read_text().strip().split("\n"))
+
+
+def adjacent_indices(given_number: ItemPosition):
+    number_width = len(str(given_number.value))
+    row_above = {
+        (x, given_number.y - 1)
+        for x in range(given_number.x - 1, given_number.x + number_width + 1)
+    }
+
+    same_row = {
+        (given_number.x - 1, given_number.y),
+        (given_number.x + number_width, given_number.y)
+    }
+
+    row_below = {
+        (x, given_number.y + 1)
+        for x in range(given_number.x - 1, given_number.x + number_width + 1)
+    }
+
+    adjacent = row_above | same_row | row_below
+    return {(x, y) for x, y in adjacent if x >= 0 and y >= 0}
+
+
+def adjacent_symbol(number, schematic):
+    for x, y in adjacent_indices(number):
+        try:
+            character = schematic[y][x]
+        except IndexError:
+            continue
+        if character not in "0123456789.":
+            return ItemPosition(character, x, y)
+    return None
+
+
+def numbers_in_schematic(schematic: tuple[str]):
+    numbers = set()
+    for y, row in enumerate(schematic):
+        for number_match in NUMBER_PATTERN.finditer(row):
+            value = int(number_match.group(1))
+            numbers.add(ItemPosition(value, number_match.start(), y))
+    return numbers
+
+
+def cogs_in_schematics(schematic):
+    numbers = numbers_in_schematic(schematic)
+
+    cog_candidates = {}
+    cogs = set()
+    for number in numbers:
+        symbol = adjacent_symbol(number, schematic)
+        if symbol and symbol.value == "*":
+            if symbol in cog_candidates:
+                gear_ratio = number.value * cog_candidates[symbol].value
+                cogs.add(ItemPosition(gear_ratio, symbol.x, symbol.y))
+            else:
+                cog_candidates[symbol] = number
+    return cogs
+
+
+def solve_part_one(input_path: Path):
+    schematic = parse_input(input_path)
+    numbers = numbers_in_schematic(schematic)
+    parts = [
+        number.value for number in numbers if adjacent_symbol(number, schematic)
+    ]
+    return sum(parts)
+
+
+def solve_part_two(input_path: Path):
+    schematic = parse_input(input_path)
+    cogs = cogs_in_schematics(schematic)
+    return sum(cog.value for cog in cogs)
+
+
+
+
+    return 0
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_3.py
@@ -0,0 +1,96 @@
+from pathlib import Path
+
+import pytest
+from aoc2023 import day_3
+from aoc2023.day_3 import ItemPosition
+
+
+def test_parse_input_returns_tuple_of_strings():
+    given_path = Path("test_input/test_input_3_1.txt")
+    parsed_schematic = day_3.parse_input(given_path)
+
+    assert parsed_schematic
+    assert isinstance(parsed_schematic, tuple)
+    assert len(parsed_schematic) == 10
+    assert all(len(row) == 10 for row in parsed_schematic)
+
+
+@pytest.mark.parametrize(
+    "given_number, expected_indices",
+    (
+        (ItemPosition(1, 0, 0), {(0, 1), (1, 0), (1, 1)}),  # Corner case
+        (ItemPosition(12, 0, 0), {(2, 0), (0, 1), (1, 1), (2, 1)}),
+        (
+                ItemPosition(3, 1, 1),
+                {(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)}
+        ),
+    )
+)
+def test_adjacent_indices_returns_indices_around_number(given_number, expected_indices):
+    assert day_3.adjacent_indices(given_number) == expected_indices
+
+@pytest.mark.parametrize(
+    "given_number, expected_symbol",
+    (
+        (ItemPosition(467, 0, 0), ItemPosition("*", 3, 2)),
+        (ItemPosition(114, 5, 0), None),
+        (ItemPosition(617, 0, 4), ItemPosition("*", 3, 4)),
+        (ItemPosition(592, 2, 6), ItemPosition("+", 5, 5)),
+        (ItemPosition(58, 7, 5), None),
+    )
+)
+def test_adjacent_symbol_finds_expected_symbol(
+        given_number: ItemPosition, expected_symbol
+):
+    given_path = Path("test_input/test_input_3_1.txt")
+    given_schematic = day_3.parse_input(given_path)
+
+    assert day_3.adjacent_symbol(given_number, given_schematic) == expected_symbol
+
+
+def test_find_numbers_in_schematic():
+    given_path = Path("test_input/test_input_3_1.txt")
+    given_schematic = day_3.parse_input(given_path)
+
+    numbers = day_3.numbers_in_schematic(given_schematic)
+
+    expected_numbers = {
+        ItemPosition(467, 0, 0),
+        ItemPosition(114, 5, 0),
+        ItemPosition(35, 2, 2),
+        ItemPosition(633, 6, 2),
+        ItemPosition(617, 0, 4),
+        ItemPosition(58, 7, 5),
+        ItemPosition(592, 2, 6),
+        ItemPosition(755, 6, 7),
+        ItemPosition(664, 1, 9),
+        ItemPosition(598, 5, 9),
+    }
+
+    assert numbers == expected_numbers
+
+
+def test_cogs_in_schematic_finds_all_cogs():
+    given_path = Path("test_input/test_input_3_1.txt")
+    given_schematic = day_3.parse_input(given_path)
+
+    cogs = day_3.cogs_in_schematics(given_schematic)
+
+    expected_cogs = {
+        ItemPosition(16345, 3, 1),
+        ItemPosition(451490, 5, 8),
+    }
+
+    assert cogs == expected_cogs
+
+
+def test_solving_part_one_gives_expected_value():
+    given_input_path = Path("test_input/test_input_3_1.txt")
+    answer = day_3.solve_part_one(given_input_path)
+    assert answer == 4361
+
+
+def test_solving_part_two_gives_expected_value():
+    given_input_path = Path("test_input/test_input_3_1.txt")
+    answer = day_3.solve_part_two(given_input_path)
+    assert answer == 467835
