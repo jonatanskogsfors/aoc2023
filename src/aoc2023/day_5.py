f2138152afcce708ccac90a0762ce12802c1e756
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2023-12-05 18:43:25 +0100

    Day 5: * WIP

--- src/aoc2023/day_5.py
+++ src/aoc2023/day_5.py
@@ -1,29 +1,179 @@
+import itertools
+from dataclasses import dataclass
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, Self
 
 
-class CategoryRange(NamedTuple):
+def main():
+    input_path = Path("input/input_5.txt")
+    print(solve_part_one(input_path))
+    print(solve_part_two(input_path))
+
+
+@dataclass
+class CategoryRange:
     destination: int
     source: int
     length: int
 
+    @property
+    def destination_end(self) -> int:
+        return self.destination + self.length - 1
+
+    @property
+    def source_end(self) -> int:
+        return self.source + self.length - 1
+
+    def __hash__(self):
+        return hash((self.destination, self.source, self.length))
+
+
+def split_ranges(
+    left_range: CategoryRange, right_range: CategoryRange
+) -> list[CategoryRange]:
+    left_start_inside_right = (
+        right_range.source <= left_range.destination <= right_range.source_end
+    )
+    left_end_inside_right = (
+        right_range.source <= left_range.destination_end <= right_range.source_end
+    )
+    ranges_overlap = left_start_inside_right or left_end_inside_right
+
+    if not ranges_overlap:
+        return None
+
+    a_length = abs(right_range.source - left_range.destination)
+    b_length = (
+        min(left_range.destination_end, right_range.source_end)
+        - max(right_range.source, left_range.destination)
+        + 1
+    )
+    c_length = abs(right_range.source_end - left_range.destination_end)
+
+    if left_range.destination < right_range.source:
+        # Left, no overlap
+        a_source = left_range.source
+        a_destination = left_range.destination
+
+        # Middle, overlap
+        b_source = left_range.source + a_length
+        b_destination = right_range.destination
+
+    elif left_range.destination >= right_range.source:
+        a_source = right_range.source
+        a_destination = right_range.destination
+
+        # Middle, overlap
+        b_source = right_range.source + a_length
+        b_destination = right_range.destination + a_length
+    else:
+        raise Exception("OOPS")
+
+    if left_range.destination_end < right_range.source_end:
+        c_source = right_range.source_end - c_length + 1
+        c_destination = right_range.destination + b_length
+
+    elif left_range.destination_end >= right_range.source_end:
+        c_source = left_range.source + b_length
+        c_destination = right_range.source_end + 1
+    else:
+        raise Exception("OOPS")
+
+    new_ranges = [CategoryRange(b_destination, b_source, b_length)]
+    if a_length:
+        new_ranges.append(CategoryRange(a_destination, a_source, a_length))
+
+    if c_length:
+        new_ranges.append(CategoryRange(c_destination, c_source, c_length))
+
+    return new_ranges
+
+
+@dataclass
+class SeedRange:
+    start: int
+    length: int
+
+    @property
+    def end(self) -> int:
+        return self.start + self.length - 1
+
+    def __hash__(self):
+        return hash((self.start, self.length))
+
 
 class CategoryMap:
     def __init__(self, *map_ranges: CategoryRange):
-        self._map_ranges = map_ranges
+        self._map_ranges = list(map_ranges)
 
-    def convert(self, source: int) -> int:
+    def convert(self, source: int):
         for map_range in self._map_ranges:
-            if map_range.source <= source <= map_range.source + map_range.length:
+            if map_range.source <= source < map_range.source + map_range.length:
                 difference = source - map_range.source
+                # print(f"{source} -> {map_range.destination + difference}")
                 return map_range.destination + difference
+        # print(f"{source} -> {source}")
         return source
+
+    def combine(self, other_map: Self):
+        combined_ranges = []
+        unchecked_ranges = other_map._map_ranges
+        while unchecked_ranges:
+            unchecked_range = unchecked_ranges.pop()
+            range_split = False
+            for map_range in self._map_ranges:
+                if new_ranges := split_ranges(map_range, unchecked_range):
+                    unchecked_ranges += new_ranges
+                    range_split = True
+                    self._map_ranges.remove(map_range)
+                    break
+            if not range_split:
+                combined_ranges.append(unchecked_range)
+
+        self._map_ranges = combined_ranges
+
 
 def parse_input(input_path: Path):
     raw_input = input_path.read_text()
-    seed_input_categories_input = raw_input.split("\n", 1)
-    seeds = map(
-        int, seed_input.split(":")[1].strip().split()
-    )
-    for category in
+    seed_input, categories_input = raw_input.split("\n", 1)
+    seeds = map(int, seed_input.split(":")[1].strip().split())
+    category_map = None
+    for category in categories_input.strip().split("\n\n"):
+        header, *ranges = category.split("\n")
+        new_map = CategoryMap(
+            *[CategoryRange(*map(int, range.split())) for range in ranges]
+        )
+        if category_map is None:
+            category_map = new_map
+        else:
+            category_map.combine(new_map)
+    return seeds, category_map
 
+
+def seeds_to_ranges(seeds):
+    return [SeedRange(start, length) for start, length in itertools.batched(seeds, 2)]
+
+
+def solve_part_one(input_path):
+    seeds, category_map = parse_input(input_path)
+    corresponding_numbers = []
+    for seed in seeds:
+        location = category_map.convert(seed)
+        corresponding_numbers.append(location)
+    return min(corresponding_numbers)
+
+
+def solve_part_two(input_path):
+    seeds, category_map = parse_input(input_path)
+    seed_ranges = seeds_to_ranges(seeds)
+    corresponding_numbers = []
+    for seed_range in seed_ranges:
+        print(seed_range.length)
+        for seed in range(seed_range.start, seed_range.start + seed_range.length):
+            location = category_map.convert(seed)
+            corresponding_numbers.append(location)
+    return min(corresponding_numbers)
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_5.py
+++ tests/test_5.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 from aoc2023 import day_5
-from aoc2023.day_5 import CategoryRange, CategoryMap
+from aoc2023.day_5 import CategoryRange, CategoryMap, SeedRange
 
 TEST_INPUT_DIR = Path(__file__).parent / "test_input"
 TEST_INPUT_1 = TEST_INPUT_DIR / "test_input_5_1.txt"
@@ -16,19 +16,124 @@ TEST_INPUT_1 = TEST_INPUT_DIR / "test_input_5_1.txt"
         ((CategoryRange(50, 98, 2), CategoryRange(52, 50, 48)), 14, 14),
         ((CategoryRange(50, 98, 2), CategoryRange(52, 50, 48)), 55, 57),
         ((CategoryRange(50, 98, 2), CategoryRange(52, 50, 48)), 13, 13),
-    )
+    ),
 )
-def test_convert_to_category_gives_correct_value(given_ranges, given_source, expected_destination):
-    _, category_map = CategoryMap(*given_ranges)
+def test_convert_to_category_gives_correct_value(
+    given_ranges, given_source, expected_destination
+):
+    category_map = CategoryMap(*given_ranges)
     destination = category_map.convert(given_source)
     assert destination == expected_destination
 
 
 @pytest.mark.parametrize(
-    "given_source, expected_destination",
-    ((79, 82), (14, 43), (55, 86), (13, 35))
+    "given_source, given_length, expected_source_end",
+    (
+        (0, 1, 0),
+        (1, 2, 2),
+        (2, 3, 4),
+    ),
 )
-def test_connected_maps_gives_correct_value(given_source, expected_destination):
-    connected_map = day_5.parse_input(TEST_INPUT_1)
+def test_category_range_calculates_source_end(
+    given_source: int, given_length: int, expected_source_end: int
+):
+    given_range = CategoryRange(1337, given_source, given_length)
+    assert given_range.source_end == expected_source_end
+
+
+@pytest.mark.parametrize(
+    "given_destination, given_length, expected_destination_end",
+    (
+        (0, 1, 0),
+        (1, 2, 2),
+        (2, 3, 4),
+    ),
+)
+def test_category_range_calculates_destination_end(
+    given_destination: int, given_length: int, expected_destination_end: int
+):
+    given_range = CategoryRange(given_destination, 1337, given_length)
+    assert given_range.destination_end == expected_destination_end
+
+    @pytest.mark.parametrize(
+        "given_start, given_length, expected_end",
+        (
+            (0, 1, 0),
+            (1, 2, 2),
+            (2, 3, 4),
+        ),
+    )
+    def test_seed_range_calculates_end(
+        given_start: int, given_length: int, expected_end: int
+    ):
+        given_range = SeedRange(given_start, given_length)
+        assert given_range.end == expected_end
+
+
+@pytest.mark.parametrize(
+    "given_source, expected_destination", ((79, 82), (14, 43), (55, 86), (13, 35))
+)
+def test_combined_maps_gives_correct_value(given_source, expected_destination):
+    _, connected_map = day_5.parse_input(TEST_INPUT_1)
     destination = connected_map.convert(given_source)
     assert destination == expected_destination
+
+
+@pytest.mark.parametrize(
+    "given_left_range, given_right_range, expected_ranges",
+    (
+        (
+            CategoryRange(1, 1, 4),
+            CategoryRange(3, 3, 4),
+            {CategoryRange(1, 1, 2), CategoryRange(3, 3, 2), CategoryRange(5, 5, 2)},
+        ),
+        (
+            CategoryRange(51, 1, 10),
+            CategoryRange(100, 60, 9),
+            {CategoryRange(51, 1, 9), CategoryRange(100, 10, 1), CategoryRange(101, 61, 8)},
+        ),
+        (
+            CategoryRange(10, 100, 20),
+            CategoryRange(78, 8, 5),
+            {CategoryRange(13, 103, 17), CategoryRange(80, 10, 3), CategoryRange(78, 8, 2)},
+        ),
+    ),
+)
+def test_split_overlapping_ranges(given_left_range, given_right_range, expected_ranges):
+    disjunct_ranges = day_5.split_ranges(given_left_range, given_right_range)
+    assert set(disjunct_ranges) == expected_ranges
+
+
+def test_combined_maps_can_convert_values():
+    given_map_a = CategoryMap(CategoryRange(47, 32, 10), CategoryRange(1, 40, 5))
+
+    given_map_b = CategoryMap(CategoryRange(98, 2, 7))
+
+    given_map_a.combine(given_map_b)
+
+    given_source = 42
+    expected_destination = 99
+
+    destination = given_map_a.convert(given_source)
+    assert destination == expected_destination
+
+
+@pytest.mark.parametrize(
+    "given_seeds, expected_ranges",
+    (((79, 14, 55, 13), {SeedRange(79, 14), SeedRange(55, 13)}),),
+)
+def test_seeds_to_range_returns_expected_range(given_seeds, expected_ranges):
+    seed_ranges = day_5.seeds_to_ranges(given_seeds)
+    assert set(seed_ranges) == expected_ranges
+
+
+def test_solving_part_one_gives_expected_value():
+    answer = day_5.solve_part_one(TEST_INPUT_1)
+    expected_answer = 35
+    assert answer == expected_answer
+
+
+def test_solving_part_two_gives_expected_value():
+    answer = day_5.solve_part_two(TEST_INPUT_1)
+    expected_answer = 46
+    assert answer == expected_answer
