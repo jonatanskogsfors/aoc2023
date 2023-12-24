945d73588ef5f3fe590124c5d73d71ae3154e0bf
Jonatan Skogsfors <jonatan.skogsfors@mindroad.se>
2023-12-24 01:05:44 +0100

    Day 18: **

--- src/aoc2023/day_18.py
+++ src/aoc2023/day_18.py
@@ -1,5 +1,6 @@
 import collections
 import re
+from itertools import pairwise
 from pathlib import Path
 
 from aoc2023.direction import Direction
@@ -33,6 +34,7 @@ def parse_input(input_path: Path, bug_fix=False):
             dig_plan.append((direction, length, color))
     return dig_plan
 
+
 def create_bounding_box(dig_plan):
     current_x = 0
     current_y = 0
@@ -45,19 +47,19 @@ def create_bounding_box(dig_plan):
     min_y = min(y_values)
     height = max(y_values) - min_y + 1
     bounding_box = []
-    print(width, height)
     for _ in range(height):
         bounding_box.append("." * width)
     return bounding_box, Position(min_x, min_y)
 
 
 def find_corners(current_x, current_y, dig_plan):
-    corners = []
+    corners = [Position(current_x, current_y)]
     for direction, length, _ in dig_plan:
         current_y += direction.delta().y * length
         current_x += direction.delta().x * length
         corners.append(Position(current_x, current_y))
     return corners
+
 
 def create_outline(dig_plan):
     lagoon, offset = create_bounding_box(dig_plan)
@@ -66,6 +68,8 @@ def create_outline(dig_plan):
         lagoon, position = dig_trench(lagoon, position, direction, length)
 
     return lagoon
+
+
 def dig_trench(lagoon, position, direction, length):
     editable_lagoon = tuple(list(row) for row in lagoon)
     editable_lagoon[position.y][position.x] = "#"
@@ -99,25 +103,41 @@ def flood_fill(lagoon, start_position):
     return tuple("".join(row) for row in editable_lagoon)
 
 
+def faux_fill(dig_plan):
+    corners = find_corners(0, 0, dig_plan)
+
+    # Pixel shoelace formula
+    criss = 0
+    cross = 0
+    for left, right in pairwise(reversed(corners)):
+        criss += right.x * left.y
+        cross += (left.x - 0) * (right.y - 0)
+
+    inner_area = abs(criss - cross) // 2
+    boundary = sum(length for _, length, _ in dig_plan)
+    return inner_area + boundary // 2 + 1
+
+
 def is_inside(lagoon, position) -> bool:
-    return len(
-        [
-            boundary
-            for boundary in lagoon[position.y][:position.y].split(".")
-            if boundary == "#"
-        ]
-    ) % 2 == 1
+    return (
+        len(
+            [
+                boundary
+                for boundary in lagoon[position.y][: position.y].split(".")
+                if boundary == "#"
+            ]
+        )
+        % 2
+        == 1
+    )
 
 
 def instruction_from_color(color):
     length = int(color[1:-1], 16)
     direction_index = int(color[-1], 16)
-    direction = [
-        Direction.RIGHT,
-        Direction.DOWN,
-        Direction.LEFT,
-        Direction.UP
-    ][direction_index]
+    direction = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP][
+        direction_index
+    ]
     return direction, length
 
 
@@ -137,19 +157,8 @@ def solve_part_one(input_path: Path):
 
 def solve_part_two(input_path: Path):
     dig_plan = parse_input(input_path, bug_fix=True)
-    outline = create_outline(dig_plan)
-    height = len(outline)
-    width = len(outline[0])
-    start_position = Position(width // 2, height // 2)
-    while not is_inside(outline, start_position):
-        start_position += Direction.LEFT.delta()
-    if start_position.x < 0:
-        raise Exception("Oops")
-    lagoon = flood_fill(outline, start_position)
-    return sum(row.count("#") for row in lagoon)
+    return faux_fill(dig_plan)
 
 
 if __name__ == "__main__":
     main()
-
-
